ba50a419fb1e235ac11142a4b16ddc2a8599d614
Johann Maximilian Goldacker <johann.goldacker@student.uni-halle.de>
2019-05-23 12:00:26 +0200

    Model and List/Table for elements implemented. New index.html working with Main.js

--- web-app/src/Main.elm
+++ web-app/src/Main.elm
@@ -1,33 +1,96 @@
 module Main exposing (..)
 
 import Browser exposing (sandbox)
-import Html exposing (Html, button, div, text)
+import Html exposing (Html, button, div, text, table, td, th, tr)
+import Html.Attributes exposing (class, id)
 import Html.Events exposing (onClick)
+import List exposing (append)
 
 main =
-  Browser.sandbox { init = initModel, view = view, update = update }
+  Browser.sandbox { init = initFragebogen, view = view, update = update }
 
+--types
 type Msg = Increment | Decrement
 
-type alias Model = 
-    { tmp : Int }
+type alias Fragebogen = 
+    {   elements : List FB_element,
+        tmp : Int }
 
-initModel : Model 
-initModel = 
-    { tmp = 0 }
+type FB_element 
+    = Anmerkung {   id : Int, 
+                    text : String }
 
-update : Msg -> Model -> Model
-update msg model =
+    | Frage {   id : Int, 
+                text : String, 
+                antworten : List Antwort, 
+                hinweis : String, 
+                typ : String }
+
+type alias Antwort = 
+    {   id : Int,
+        text : String,
+        typ : String }
+
+--Init
+initFragebogen : Fragebogen 
+initFragebogen = 
+    { elements = [initFrage], tmp = 0 }
+
+--Beispielfrage
+initFrage : FB_element
+initFrage = 
+    Frage {id = 0, text = "Wie geht's?", antworten = [], hinweis = "Das ist eine Frage", typ = "Typ 1"}
+
+
+--Update logic
+update : Msg -> Fragebogen -> Fragebogen
+update msg fragebogen =
     case msg of
         Increment ->
-            { model | tmp = model.tmp + 1 }
+            { fragebogen | tmp = fragebogen.tmp + 1 }
         Decrement ->
-            { model | tmp = model.tmp - 1 }
+            { fragebogen | tmp = fragebogen.tmp - 1 }
 
-view : Model -> Html Msg
-view model =
+--View
+
+view : Fragebogen -> Html Msg
+view fragebogen =
   div []
-    [ button [ onClick Decrement ] [ text "-" ]
-    , div [] [ text (String.fromInt model.tmp) ]
-    , button [ onClick Increment ] [ text "+" ]
+    [ table [ class "table is-striped" ] (fragenTable fragebogen)
     ]
+
+fragenTable : Fragebogen -> List (Html Msg)
+fragenTable fragebogen =
+    append [ tableHead ] (List.indexedMap getTable fragebogen.elements)
+
+tableHead : Html Msg
+tableHead =
+    tr [] [ 
+        th [] [ 
+            text "id" 
+        ], 
+        th [] [ 
+            text "Foo" 
+        ], 
+        th [] [ 
+            text "Bar" 
+        ]
+    ]
+
+getTable : Int -> FB_element -> Html msg
+getTable index element =
+    case element of
+        Anmerkung a ->
+            tr [ id (String.fromInt index) ]
+                [ td [] [ text (String.fromInt index) ]
+                , td [] [ text "foo" ]
+                , td [] [ text "bar" ]
+                ]
+
+        Frage f->
+            tr [ id (String.fromInt index) ]
+                [ td [] [ text (String.fromInt index) ]
+                , td [] [ text "foo" ]
+                , td [] [ text "bar" ]
+                ]
+

519564280a778410f4b17cc43134dc246eadcf10
Johann Maximilian Goldacker <goldacker.johann@outlook.de>
2019-11-13 19:01:52 +0100

    Neue Eingabemethode für Reminder Times

--- web-app/src/elmPort.js
+++ web-app/src/elmPort.js
@@ -2,6 +2,34 @@ var app = Elm.Main.init({
     node: document.getElementById('elm')
   });
 
+function appendToTimesTable() {
+    var table = document.getElementById("reminderTimesTable").getElementsByTagName('tbody')[0];
+    var row = table.insertRow(0);
+    var cell = row.insertCell(0);
+    var value = document.getElementById("basicDate").value;
+    cell.innerHTML = value;
+}
+
+function resetTimesTable() {
+    var table = document.getElementById("reminderTimesTable").getElementsByTagName('tbody')[0];
+    table.innerHTML = "";
+}
+
+function connectReminderTimes() {
+    var table = document.getElementById("reminderTimesTable").getElementsByTagName('tbody')[0];
+    var allTimes;
+
+    if (table.rows.length > 0) {
+        allTimes = table.rows[0].innerText;
+    }
+
+    for (var i = 1; i < table.rows.length; i++) {
+        allTimes += ';' + table.rows[i].innerText;
+    }
+
+    sendToElm(allTimes, "reminderTime");
+}
+
 function sendToElm(value, dateTimePicker) {
 
     if (dateTimePicker == "viewingTime") {
@@ -45,6 +73,7 @@ function closeDTPModal(value) {
     if (value == "reminderTime") {
         var modal = document.getElementById("modalReminderTime");
         modal.classList.remove("is-active");
+        connectReminderTimes();
     }
 
     if (value == "editTime") {

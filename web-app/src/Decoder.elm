57deef546d339536da5d0b76da1a3dcf4c4e6704
Johann Maximilian Goldacker <johann.goldacker@student.uni-halle.de>
2019-11-15 13:10:06 +0100

    Fragen werden auch als solche dekodiert

--- web-app/src/Decoder.elm
+++ web-app/src/Decoder.elm
@@ -108,13 +108,13 @@ questionDecoder =
         |> required "text" Decode.string
         |> required "answers" (Decode.list answerDecoder)
         |> required "hint" Decode.string
-        |> required "question_type" Decode.string
-        |> required "question_time" Decode.string
-        |> required "table_size" Decode.int
-        |> required "top_text" Decode.string
-        |> required "right_text" Decode.string
-        |> required "bottom_text" Decode.string
-        |> required "left_text" Decode.string
+        |> required "questionType" Decode.string
+        |> required "questionTime" Decode.string
+        |> required "tableSize" Decode.int
+        |> required "topText" Decode.string
+        |> required "rightText" Decode.string
+        |> required "bottomText" Decode.string
+        |> required "leftText" Decode.string
         |> Decode.map Question
 
 

3a3d48fc21affbbba12e688a2658f773a7c76f97
Marcus Gagelmann <marcus.gagelmann@student.uni-halle.de>
2019-11-22 17:31:24 +0100

    Replace Main.java

--- converter/src/main/java/converter/Main.java
+++ converter/src/main/java/converter/Main.java
@@ -4,21 +4,42 @@ import java.nio.file.Paths;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
+import java.io.PrintWriter;
 
-public class JSON_to_CSV {
-
+public class Main {
+	//Dateipfad mit "\\" zur Ordnertrennung und Name der JSON in args[0] übergeben
+	//z.B. C:\\User\\Eclipse\\exampleQuestionnaire.json
+	//Dateipfad mit "\\" zur Ordnertrennung und Name der Zieldatei in args[1] übergeben
+	//z.B. C:\\User\\Eclipse\\test.csv
 	public static void main(String[] args) {
 		
 		File file = new File(args[0]);
 		try {
 			String content = new String(Files.readAllBytes(Paths.get(file.toURI())), "UTF-8");
-			JSONObject json = new JSONObject(content);
+			JSONObject questionnaire = new JSONObject(content);
 
-			System.out.println(json.getInt("id"));
+			//JSON lesen Beispiele
+			System.out.println(questionnaire.getInt("id"));
+			JSONArray elements = questionnaire.getJSONArray("elements");
+			JSONObject question_1 = elements.getJSONObject(0);
+			System.out.println(question_1.getString("text"));
 			
-			JSONArray json2 = json.getJSONArray("elements");
-			JSONObject json3 = json2.getJSONObject(0);
-			System.out.println(json3.getString("text"));
+			//CSV schreiben Beispiel
+			PrintWriter pw = new PrintWriter(new File(args[1]));
+			StringBuilder sb = new StringBuilder();
+			
+			sb.append("id;");
+			sb.append(questionnaire.getInt("id"));
+			sb.append("\r\n");
+			sb.append("title;");
+			sb.append(questionnaire.getString("title"));
+			sb.append("\r\n");
+			sb.append("priority;");
+			sb.append(questionnaire.getInt("priority"));
+			
+			pw.write(sb.toString());
+			pw.close();
+			System.out.println("file created");
 			
 		} catch(Exception e) {
 			

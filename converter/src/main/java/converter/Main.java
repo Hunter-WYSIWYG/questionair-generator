a993da9f28482961f48ad8cb88d51cb14d9edcec
Marcus Gagelmann <marcus.gagelmann@student.uni-halle.de>
2019-11-22 16:33:17 +0100

    Parseranfänge mit Beispielen zur späteren Fortsetzung (WIP)

--- converter/src/main/java/converter/Main.java
+++ converter/src/main/java/converter/Main.java
@@ -1,7 +1,29 @@
-package converter;
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("foo bar");
-    }
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class JSON_to_CSV {
+
+	public static void main(String[] args) {
+		
+		File file = new File(args[0]);
+		try {
+			String content = new String(Files.readAllBytes(Paths.get(file.toURI())), "UTF-8");
+			JSONObject json = new JSONObject(content);
+
+			System.out.println(json.getInt("id"));
+			
+			JSONArray json2 = json.getJSONArray("elements");
+			JSONObject json3 = json2.getJSONObject(0);
+			System.out.println(json3.getString("text"));
+			
+		} catch(Exception e) {
+			
+		}
+
+	}
+
 }

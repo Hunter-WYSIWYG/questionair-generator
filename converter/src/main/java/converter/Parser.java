35f43ec489e353f2f73ed2152e58c421e7ba1754
Marcus Gagelmann <marcus.gagelmann@student.uni-halle.de>
2019-11-28 19:46:13 +0100

    Parser vollständig

--- converter/src/main/java/converter/Parser.java
+++ converter/src/main/java/converter/Parser.java
@@ -20,31 +20,65 @@ public class Parser {
 		File file = new File(sourceFile);
 		try {
 			String content = new String(Files.readAllBytes(Paths.get(file.toURI())), "UTF-8");
-			JSONObject questionnaire = new JSONObject(content);
-
-			//JSON lesen Beispiele
-			System.out.println(questionnaire.getInt("id"));
-			JSONArray elements = questionnaire.getJSONArray("elements");
-			JSONObject question_1 = elements.getJSONObject(0);
-			System.out.println(question_1.getString("text"));
 			
-			//CSV schreiben Beispiel
-			PrintWriter pw = new PrintWriter(new File(targetFile));
+			JSONArray questionnaire = new JSONArray(content);	//JSON Objekte
+			JSONObject currentQuestion;
+			JSONArray currentAnswers;
+			JSONObject currentAnswer;
+			int noteCounter = 0;	//Notizanzahl wird von question_id abgezogen um Fragenummer zu erhalten
+			
+			PrintWriter pw = new PrintWriter(new File(targetFile));	//Tools zur CSV-Erstellung
 			StringBuilder sb = new StringBuilder();
 			
-			sb.append("id;");
-			sb.append(questionnaire.getInt("id"));
-			sb.append("\r\n");
-			sb.append("title;");
-			sb.append(questionnaire.getString("title"));
-			sb.append("\r\n");
-			sb.append("priority;");
-			sb.append(questionnaire.getInt("priority"));
+			if (questionnaire.length()<=0) {	//leeres Array
+				sb.append("Die Liste von Fragen und Antworten ist Leer.");
+			} else {
 			
+				currentQuestion = questionnaire.getJSONObject(0);
+				
+				sb.append("Fragebogen;;");	//Titel des Fragebogens
+				sb.append(currentQuestion.getString("title_of_questionnaire"));
+				sb.append("\r\n");
+				sb.append("\r\n");
+				
+				for (int i=0;i<questionnaire.length();i++) {	//alle Fragen nacheinander auslesen
+					currentQuestion = questionnaire.getJSONObject(i);
+					currentAnswers = currentQuestion.getJSONArray("answers");
+					
+					if (currentQuestion.getString("type_of_question").equals("note")) { //Notiz? else Frage
+						noteCounter++;
+						sb.append("Notiz;;");	//Notiz schreiben
+						sb.append(currentQuestion.getString("question_text"));
+						sb.append("\r\n");
+						sb.append("\r\n");
+					} else {					//Frage + Antworten  + Antwortzeit schreiben
+						sb.append("Frage "+(currentQuestion.getInt("question_id")-noteCounter) + ";;");		//Notizanzahl wird von question_id abgezogen um Fragenummer zu erhalten
+						sb.append(currentQuestion.getString("question_text"));
+						sb.append("\r\n");
+						
+						for (int j=0; j<currentAnswers.length();j++) {
+							currentAnswer = currentAnswers.getJSONObject(j);
+							sb.append("Antwort "+currentAnswer.getInt("id") + ";;");
+							if (currentQuestion.getString("type_of_question").equals("slider")) {
+								sb.append(currentAnswer.getInt("id"));
+							} else {
+								sb.append(currentAnswer.getString("text"));
+							}
+							sb.append("\r\n");
+						}
+						sb.append("Antwortzeit;;");
+						sb.append(currentQuestion.getString("answerTime"));
+						sb.append("\r\n");
+						sb.append("\r\n");
+					}
+					
+				}
+				
+			}
 			pw.write(sb.toString());
 			pw.close();
 			System.out.println("file created");
-			
+				
 		} catch(Exception e) {
 			
 		}

a377504d526262ab5e66abee38f447c452192087
Marcus Gagelmann <marcus.gagelmann@student.uni-halle.de>
2019-12-03 15:47:45 +0100

    Replace Main.java

--- converter/src/main/java/converter/Main.java
+++ converter/src/main/java/converter/Main.java
@@ -1,22 +1,13 @@
-import java.io.File;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+package converter;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
-import java.io.PrintWriter;
+import converter.graphical.ui.GUI;
+
 
 public class Main {
 	
-	//Dateipfad mit "\\" zur Ordnertrennung und Name der JSON in args[0] übergeben
-	//z.B. C:\\User\\Eclipse\\exampleQuestionnaire.json
-	//Dateipfad mit "\\" zur Ordnertrennung und Name der Zieldatei in args[1] übergeben
-	//z.B. C:\\User\\Eclipse\\test.csv
-	
 	public static void main(String[] args) {
 		
-		GUI newGUI = new GUI("gui", args[0], args[1]);
-
+		GUI.getInstance();
 	}
 
 }

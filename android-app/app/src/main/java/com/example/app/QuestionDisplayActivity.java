f18ad032472b7b5245983728447ce8ffc9d29ffd
nina <ninams99@gmail.com>
2019-12-09 23:16:22 +0100

    added altert dialogs if back putton is pushed

--- android-app/app/src/main/java/com/example/app/QuestionDisplayActivity.java
+++ android-app/app/src/main/java/com/example/app/QuestionDisplayActivity.java
@@ -1,8 +1,11 @@
 package com.example.app;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.GravityCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
 import android.widget.LinearLayout;
@@ -92,4 +95,33 @@ public class QuestionDisplayActivity extends AppCompatActivity {
 		return returnString.toString ();
 	}
 	
+	// create popup if back button is pressed
+	public void onBackPressed() {
+		AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+		alertDialog.setTitle("");
+		alertDialog.setMessage("Wollen Sie den Fragebogen verlassen?");
+		alertDialog.setCancelable(true);
+		
+		alertDialog.setPositiveButton(
+			"Ja",
+			new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog, int id) {
+					dialog.cancel();
+					Intent intent = new Intent (QuestionDisplayActivity.this, MainActivity.class);
+					startActivity(intent);
+					finish();
+				}
+			});
+		
+		alertDialog.setNegativeButton(
+			"Abbrechen",
+			new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog, int id) {
+					dialog.cancel();
+				}
+			});
+		
+		AlertDialog alert = alertDialog.create();
+		alert.show();
+	}
 }

00c44bdb1f4d7eb12de0d11d6c9def096bfc64c2
nina <ninams99@gmail.com>
2019-12-10 22:31:23 +0100

    merge conflicts behoben

--- android-app/app/src/main/java/com/example/app/question/Question.java
+++ android-app/app/src/main/java/com/example/app/question/Question.java
@@ -1,6 +1,7 @@
 package com.example.app.question;
 
 import com.example.app.answer.Answer;
+import com.example.app.answer.Condition;
 import com.google.gson.*;
 import com.google.gson.annotations.JsonAdapter;
 import com.google.gson.annotations.SerializedName;
@@ -13,7 +14,7 @@ import java.util.List;
 @JsonAdapter (QuestionAdapter.class) // -> using QuestionAdapter to convert from or to JSON
 public abstract class Question implements Serializable {
 	
-	public final int questionID;
+	public final int id;
 	
 	public static final String TYPE_JSON_NAME = "type";
 	@SerializedName (TYPE_JSON_NAME)
@@ -26,7 +27,7 @@ public abstract class Question implements Serializable {
 	public final String hint;
 	
 	// constructor
-	public Question (int id, QuestionType type, List<Condition> conditions, String questionText, String hint) {
+	public Question(int id, QuestionType type, List<Condition> conditions, String questionText, String hint) {
 		this.id = id;
 		this.type = type;
 		this.questionText = questionText;

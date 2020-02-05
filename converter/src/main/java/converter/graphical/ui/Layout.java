18b0a3ef72c9792fd56aa64da24cecf6a40e2a44
Johann Maximilian Goldacker <johann.goldacker@student.uni-halle.de>
2020-02-05 17:40:35 +0100

    Hochladen-Button offensichtlicher gemacht

--- converter/src/main/java/converter/graphical/ui/Layout.java
+++ converter/src/main/java/converter/graphical/ui/Layout.java
@@ -1,16 +1,13 @@
 package converter.graphical.ui;
 
 import converter.adb.ADB;
-import converter.graphical.events.ComboBoxChanged;
-import converter.graphical.events.InstallBtnClick;
-import converter.graphical.events.RefreshBtnClick;
-import converter.graphical.events.SaveBtnEvent;
+import converter.graphical.events.*;
 import converter.graphical.table.CsvTable;
 
 import javax.swing.*;
 import java.awt.*;
 
-import static java.awt.GridBagConstraints.FIRST_LINE_START;
+import static java.awt.GridBagConstraints.*;
 
 /**
  * Represents the layout of the GUI.
@@ -36,6 +33,7 @@ public class Layout extends JPanel {
         updateJsonComboBox();
         constraints.gridx = 0;
         constraints.gridy = 0;
+        constraints.gridwidth = 3;
         constraints.insets = new Insets(10,10,0,10);
         add(jsonComboBox, constraints);
 
@@ -50,25 +48,37 @@ public class Layout extends JPanel {
         scrollPane.setPreferredSize(new Dimension(360,200));
         constraints.gridx = 0;
         constraints.gridy = 2;
-        constraints.gridwidth = 3;
         constraints.weighty = 1.0;
         add(scrollPane, constraints);
 
         JButton saveButton = new JButton("Speichern");
         constraints.gridx = 0;
         constraints.gridy = 3;
-        constraints.weighty = 0;
         constraints.anchor = FIRST_LINE_START;
+        constraints.gridwidth = 1;
+        constraints.weighty = 0;
         constraints.insets = new Insets(10, 10, 10, 0);
         saveButton.addActionListener(new SaveBtnEvent());
         add(saveButton, constraints);
 
-        JButton install = new JButton("App instalieren");
-        install.addActionListener(new InstallBtnClick());
+        JButton upload = new JButton("Bogen hochladen");
+        upload.addActionListener(new FragebogenHochladenClick());
         constraints.gridx = 0;
         constraints.gridy = 4;
-        constraints.insets = new Insets(0, 10, 10, 0);
+        constraints.anchor = FIRST_LINE_START;
+        constraints.weightx = 0.5;
+        constraints.insets = new Insets(0, 10, 10, 10);
+        add(upload, constraints);
+
+        JButton install = new JButton("App installieren");
+        install.addActionListener(new InstallBtnClick());
+        constraints.gridx = 1;
+        constraints.gridy = 4;
+        constraints.anchor = FIRST_LINE_END;
+        constraints.weightx = 0.5;
+        constraints.insets = new Insets(0, 0, 10, 10);
         add(install, constraints);
+
     }
 
     public static boolean isNotInstantiated() {
